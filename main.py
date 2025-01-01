f633f803d085eb3ba833b7f799e2aa51ac0153ad
hortushonu <james.eric.knight@gmail.com>
2025-01-01 15:32:14 -0500

    delta time (dt) and made game loop more concise

--- main.py
+++ main.py
@@ -6,18 +6,23 @@ from constants import *
 
 def main():
     pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    running = True
+    dt = 0
+
     print("Starting asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen width: {SCREEN_HEIGHT}")
 
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
-    while True:
+    while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill((0,0,0), rect=None, special_flags=0)
+        screen.fill(("black"), rect=None, special_flags=0)
         pygame.display.flip()
+        dt = clock.tick(60) / 1000
 
 if __name__ == "__main__":
     main()
